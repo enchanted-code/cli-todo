d56e623c13e97f8172b2125cf959dfe34ef39de1
Leo Spratt <enchant97@users.noreply.github.com>
2021-08-08 14:30:39 +0100

    add basic configuration

--- src/c/main.c
+++ src/c/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TODO_FP "todos.txt"
+#define DEFAULT_TODO_FP "todos.txt"
 
 void show_no_args_message() {
   fprintf(stderr, "Arguments need to be supplied. Use --help.");
@@ -14,24 +14,27 @@ void show_help_message() {
   puts("Usage:");
   puts("\t(-i --interactive) open in interactive mode");
   puts("\t(-h --help) show this message");
+  puts("Configure:");
+  puts("\t TODO_FILENAME where the todo file will be");
   exit(EXIT_SUCCESS);
 }
 
-void write_todo(char *title, char *due_date) {
+void write_todo(char *title, char *due_date, char *todo_fp) {
   FILE *fp;
-  fp = fopen(TODO_FP, "a");
+  fp = fopen(todo_fp, "a");
   fprintf(fp, "title=\"%s\",due_date=%s\n", title, due_date);
   fclose(fp);
 }
 
-void read_todos() {
+void read_todos(char *todo_fp) {
   FILE *fp;
   char *line = NULL;
   size_t len = 0;
   ssize_t read;
-  fp = fopen(TODO_FP, "r");
+  fp = fopen(todo_fp, "r");
 
   if (fp == NULL) {
+    fprintf(stderr, "Todo file not found.");
     exit(EXIT_FAILURE);
   }
 
@@ -61,7 +64,7 @@ void get_stdin_line(char *str, int n) {
   str[last_i] = 0;
 }
 
-void interactive_add() {
+void interactive_add(char *todo_fp) {
   // define variables
   char title[100];
   char due_date[12];
@@ -71,19 +74,19 @@ void interactive_add() {
   printf("Due Date: ");
   get_stdin_line(due_date, 12);
   // write the todo to file
-  write_todo(title, due_date);
+  write_todo(title, due_date, todo_fp);
 }
 
-void interactive_read() { read_todos(); }
+void interactive_read(char *todo_fp) { read_todos(todo_fp); }
 
-void interactive_mode() {
+void interactive_mode(char *todo_fp) {
   char menu_choice[3];
   printf("(a)dd, (r)ead: ");
   get_stdin_line(menu_choice, 3);
   if (strcmp(menu_choice, "a") == 0) {
-    interactive_add();
+    interactive_add(todo_fp);
   } else if (strcmp(menu_choice, "r") == 0) {
-    interactive_read();
+    interactive_read(todo_fp);
   } else {
     printf("%s %s", "Unknown choice:", menu_choice);
     exit(EXIT_FAILURE);
@@ -91,12 +94,17 @@ void interactive_mode() {
 }
 
 int main(int argc, char *argv[]) {
+  char *todo_fp = getenv("TODO_FILENAME");
+  if (!todo_fp) {
+    todo_fp = DEFAULT_TODO_FP;
+  }
+
   if (argc == 1) {
     show_no_args_message();
   } else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
     show_help_message();
   } else if (strcmp(argv[1], "-i") == 0 ||
              strcmp(argv[1], "--interactive") == 0) {
-    interactive_mode();
+    interactive_mode(todo_fp);
   }
 }
