15388ae25b11354d4b64f1b08a44996bcfc6296a
Leo Spratt <enchant97@users.noreply.github.com>
2021-08-13 11:53:59 +0100

    add interactive mode & fixes

--- src/csharp/Program.cs
+++ src/csharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Globalization;
 
 namespace csharp
 {
@@ -21,6 +22,14 @@ namespace csharp
             if (args[0] == "-h" || args[0] == "--help")
             {
                 show_help_message();
+            }
+            else if (args[0] == "-i" || args[0] == "--interactive")
+            {
+                interactive_mode(todo_fn);
+            }
+            else
+            {
+                show_invalid_args_message();
             }
         }
         static private void show_no_args_message()
@@ -67,13 +76,22 @@ namespace csharp
                 Environment.Exit(1);
             }
             string due_date_str = "";
-            if (due_date != null)
-            {
-                due_date_str = due_date.ToString("%yyyy-%MM-%dd");
-            }
+            due_date_str = due_date.ToString("%yyyy-%MM-%dd");
             File.AppendAllText(
                 todo_fp,
                 String.Format("title={0},due_date={1}\n", title, due_date_str)
+            );
+        }
+        static private void write_todo(string title, string todo_fp)
+        {
+            if (!File.Exists(todo_fp))
+            {
+                Console.Error.WriteLine("Todo file not found");
+                Environment.Exit(1);
+            }
+            File.AppendAllText(
+                todo_fp,
+                String.Format("title={0},due_date=\n", title)
             );
         }
         static private void read_todos(string todo_fp)
@@ -141,17 +159,97 @@ namespace csharp
             {
                 if (currLine != selected_line)
                 {
-                    File.AppendAllText(tempPath, line);
+                    File.AppendAllText(tempPath, line + "\n");
                 }
                 currLine++;
             }
             File.Delete(todo_fp);
             File.Move(tempPath, todo_fp);
         }
-        static private void interactive_add() { }
-        static private void interactive_read() { }
-        static private void interactive_delete() { }
-        static private void interactive_mode() { }
+        static private void interactive_add(string todo_fp)
+        {
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+            Console.Write("Due Date: ");
+            string due_date_str = Console.ReadLine();
+            DateTime dueDateTime;
+            if (!String.IsNullOrEmpty(due_date_str))
+            {
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                dueDateTime = DateTime.ParseExact(due_date_str, "%yyyy-%MM-%dd", provider);
+                write_todo(title, dueDateTime, todo_fp);
+            }
+            else
+            {
+                write_todo(title, todo_fp);
+            }
+        }
+        static private void interactive_read(string todo_fp)
+        {
+            Console.Write("(a)ll, (o)ne, (c)ount: ");
+            string choice = Console.ReadLine().ToLower();
+            if (choice.StartsWith("a"))
+            {
+                read_todos(todo_fp);
+            }
+            else if (choice.StartsWith("o"))
+            {
+                Console.Write("Number: ");
+                var selected_line = int.Parse(Console.ReadLine());
+                read_todo(selected_line, todo_fp);
+            }
+            else if (choice.StartsWith("c"))
+            {
+                Console.WriteLine(count_todos(todo_fp));
+            }
+            else
+            {
+                Console.Error.WriteLine("Unknown choice: {0}", choice);
+                Environment.Exit(1);
+            }
+        }
+        static private void interactive_delete(string todo_fp)
+        {
+            Console.Write("(a)ll, (o)ne: ");
+            string choice = Console.ReadLine().ToLower();
+            if (choice.StartsWith("a"))
+            {
+                delete_todos(todo_fp, true);
+            }
+            else if (choice.StartsWith("o"))
+            {
+                Console.Write("Number: ");
+                var selected_line = int.Parse(Console.ReadLine());
+                delete_todo(selected_line, todo_fp);
+            }
+            else
+            {
+                Console.Error.WriteLine("Unknown choice: {0}", choice);
+                Environment.Exit(1);
+            }
+        }
+        static private void interactive_mode(string todo_fp)
+        {
+            Console.Write("(a)dd, (r)ead, (d)elete: ");
+            string choice = Console.ReadLine().ToLower();
+            if (choice.StartsWith("a"))
+            {
+                interactive_add(todo_fp);
+            }
+            else if (choice.StartsWith("r"))
+            {
+                interactive_read(todo_fp);
+            }
+            else if (choice.StartsWith("d"))
+            {
+                interactive_delete(todo_fp);
+            }
+            else
+            {
+                Console.Error.WriteLine("Unknown choice: {0}", choice);
+                Environment.Exit(1);
+            }
+        }
         static private void command_add_() { }
         static private void command_view() { }
         static private void command_delete() { }
